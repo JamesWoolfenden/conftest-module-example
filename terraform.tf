dcc7244a3d90c3e2c51b70533120995d981905ad
James Woolfenden <James.Woolfenden@gmail.com>
2020-10-16 10:12:36 +0100

    fmt

--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,18 @@
 
 resource "aws_security_group_rule" "my-rule" {
-    type        = "ingress"
-    cidr_blocks = ["0.0.0.0/0"]
-    to_port=80
-    from_port=80
-    protocol="http"
-    security_group_id=aws_db_security_group.my-group.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  to_port           = 80
+  from_port         = 80
+  protocol          = "http"
+  security_group_id = aws_db_security_group.my-group.id
 }
 
-resource "aws_alb_listener" "my-alb-listener"{
-    port     = "80"
-    protocol = "HTTP"
-    load_balancer_arn=""
-   default_action {
+resource "aws_alb_listener" "my-alb-listener" {
+  port              = "80"
+  protocol          = "HTTP"
+  load_balancer_arn = ""
+  default_action {
     type = "fixed-response"
 
     fixed_response {
@@ -24,8 +24,8 @@ resource "aws_alb_listener" "my-alb-listener"{
 }
 
 resource "aws_db_security_group" "my-group" {
-name="db"
-ingress {
+  name = "db"
+  ingress {
     cidr = "10.0.0.0/24"
   }
 }
